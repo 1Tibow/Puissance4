e1485586db5952f604062e51213b5aa7834b5a63
1Tibow <thibault.macquart@eleve.egd.mg>
2020-02-10 09:18:50 +0100

    fonction d'affichage

---
+++ affiche_gril.py
@@ -0,0 +1,23 @@
+
+def affiche(gril):
+    print(" ","0","1","2","3","4","5","6")
+    for i in range(6):
+        print(i,end='')
+        for z in range(7):
+            if gril[i][z]==0:
+                print("|.",end='')
+            elif gril[i][z]==1:
+                print("|x",end='')
+            else:
+                print("|0",end='')
+            
+        print("|")   
+    
+affiche([[0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 1, 0, 0, 0, 0],
+         [0, 0, 1, 0, 2, 0, 0],
+         [0, 0, 1, 0, 2, 0, 0],
+         [0, 0, 1, 0, 2, 0, 0]]) 
+    
+
